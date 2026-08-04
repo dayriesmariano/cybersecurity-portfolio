80d229886b68cfc3d9e2a7a1f72c0cc9e6b157c2
dayriesmariano <dayriesmariano5@gmail.com>
2026-08-04 08:28:08 +0800

    Portfolio

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 const container = document.getElementById("certificate-container");
 
-for(let i=1;i<=25;i++){
 
-    const card=document.createElement("div");
-
-    card.className="certificate-card";
-
-    const img=document.createElement("img");
-
-    if(i===13){
-
-        img.src=`images/${i}.png`;
-
-    }else{
-
-        img.src=`images/${i}.jpg`;
-
-    }
-
-    img.onclick=()=>window.open(img.src);
-s
-    card.appendChild(img);
-
-    container.appendChild(card);
-
-}
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
