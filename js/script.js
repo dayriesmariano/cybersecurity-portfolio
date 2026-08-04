ceb9f7e53427cc84abeac916fda0e63d3945b5b8
dayriesmariano <dayriesmariano5@gmail.com>
2026-08-04 08:11:11 +0800

    Portfolio

--- js/script.js
+++ js/script.js
@@ -25,3 +25,15 @@ s
     container.appendChild(card);
 
 }
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
